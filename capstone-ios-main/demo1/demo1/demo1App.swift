9d4fa9a1a550fd1f8a4fdcec61907ba5b334a1c3
Ivan <75654991+ibarraz5@users.noreply.github.com>
2022-12-12 13:41:54 -0700

    Add files via upload

---
+++ capstone-ios-main/demo1/demo1/demo1App.swift
@@ -0,0 +1,17 @@
+//
+//  demo1App.swift
+//  demo1
+//
+//  Created by Ivan Barraza Duran 
+//
+
+import SwiftUI
+
+@main
+struct demo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
